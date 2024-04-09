c561bcfd03ed7ce7e5f93d6d92ab116685042a5d
habberrih <abdul2013ly@gmail.com>
2024-04-09 21:11:33 +0200

    init commit

---
+++ assets/app.js
@@ -0,0 +1,111 @@
+function rand(min, max) {
+  return (
+    (Math.floor(Math.pow(10, 14) * Math.random() * Math.random()) %
+      (max - min + 1)) +
+    min
+  );
+}
+
+var numberSpin = function (selector) {
+  var element = document.getElementById(selector);
+  var factor = 10 + Math.floor(Math.random() * 10);
+  var num = 10;
+  var section = 100 / (num + 1);
+  var stopValue = 1;
+  var spin = function (flag, x) {
+    var value = element.style.transform;
+    value = value ? parseFloat(value.split("(")[1].split(")").join("")) : 0;
+    if (flag && flag == true) {
+      if (
+        stopValue != 1 &&
+        (value <= stopValue || value - section / factor <= stopValue) &&
+        typeof x != "undefined" &&
+        value >= x * -section &&
+        value <= (x - 0.5 >= 0 ? x - 0.5 : stopValue == 0 ? 0 : 0.5) * -section
+      ) {
+        element.style.transform = "translateY(" + stopValue + "%)";
+        stopValue = 1;
+        return true;
+      }
+      stopValue =
+        Math.floor(value / section) >= -num
+          ? Math.floor(value / section) * section
+          : 0;
+    }
+    if (value && value <= -(section * num)) {
+      element.style.transform = "translateY(" + section + "%)";
+      value = 0;
+    } else {
+      value -= section / factor;
+    }
+    element.style.transform = "translateY(" + value + "%)";
+    return false;
+  };
+  var spinTimer = setInterval(spin, 10);
+  function stop(delay, x) {
+    setTimeout(function () {
+      clearTimeout(spinTimer);
+      var stopTimer = setInterval(function () {
+        if (spin(true, x)) {
+          clearInterval(stopTimer);
+        }
+      }, 10);
+    }, delay);
+  }
+  return {
+    stop: function (delay, x) {
+      return stop(delay, x);
+    },
+  };
+};
+
+let header = document.querySelector("header");
+let main = document.querySelector("main");
+let numberInput = document.querySelector("#number");
+let scroll1 = document.querySelector("#scroll1");
+let drumRoll = document.querySelector("#drum_roll");
+let cheer = document.querySelector("#cheer");
+let fireworkPlaceholder = document.querySelector(".placeholder");
+let lucky = document.querySelector("#lucky");
+let slots = [...document.querySelectorAll(".slot")];
+let spinBtn = document.querySelector(".spin-button");
+let min = 0;
+let max = 0;
+
+numberInput.addEventListener("submit", (e) => {
+  e.preventDefault();
+  min = parseInt(e.target.min.value);
+  max = parseInt(e.target.max.value);
+  main.scrollIntoView({ behavior: "smooth" });
+});
+
+spinBtn.addEventListener("click", () => {
+  if (min === 0 || max === 0) {
+    alert("من فضلك قم بإدخال الحد الاعلى ");
+  } else if (slots[0].classList.contains("visible")) {
+    drumRoll.play();
+    lucky.textContent = "...الفائز هو";
+    fireworkPlaceholder.classList.remove("pyro");
+    let winner = 0;
+    while (winner === 0) {
+      winner = rand(min, max);
+    }
+    numberSpin("scroll1").stop(5000, Math.floor(winner / 10000));
+    numberSpin("scroll2").stop(6000, Math.floor((winner % 10000) / 1000));
+    numberSpin("scroll3").stop(7000, Math.floor((winner % 1000) / 100));
+    numberSpin("scroll4").stop(8000, Math.floor((winner % 100) / 10));
+    numberSpin("scroll5").stop(9000, winner % 10);
+
+    setTimeout(() => {
+      fireworkPlaceholder.classList.add("pyro");
+      lucky.textContent = "🥳🎉ألف مبارك🎉🥳";
+      drumRoll.pause();
+      cheer.play();
+    }, 9500);
+  } else {
+    lucky.style.opacity = 1;
+    slots.map((slot) => {
+      slot.classList.add("visible");
+    });
+  }
+});
